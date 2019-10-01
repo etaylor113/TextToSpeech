b2f98e62144f946f4a1181a186715eca3b25afe4
Evan Taylor <taylorevan113@gmail.com>
2019-10-01 15:48:35 -0500

    Taking use input, Speak is async

--- Program.cs
+++ Program.cs
@@ -8,134 +8,87 @@ using System.Speech.Recognition;
 using System.Speech.Synthesis;
 using System.Threading;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace TextToSpeech
 {
+    // page ref: https://www.codeproject.com/Articles/483347/Speech-recognition-speech-to-text-text-to-speech-a
     class Program
     {
-        static ManualResetEvent completed = null;
-
         static void Main(string[] args)
         {
             try
             {
-                PromptBuilder builder = new PromptBuilder();
 
-                builder.StartVoice(VoiceGender.Female);
-                builder.AppendText("Hello Caitlin.");
-                builder.EndVoice();
-
-                builder.AppendBreak(new TimeSpan(0, 0, 0, 1, 500));
-
-                builder.StartSentence();
-                builder.AppendText("I am watching you.");
-                builder.EndSentence();
-
-                builder.AppendBreak(new TimeSpan(0, 0, 1));
-
-                builder.StartSentence();
-                builder.AppendText("I see your every move.");
-                builder.EndSentence();
-
-                builder.AppendBreak(new TimeSpan(0, 0, 0, 0, 200));
-
-                builder.StartSentence();
-                builder.AppendText("Every keystroke.");
-                builder.EndSentence();
-
-                builder.AppendBreak(new TimeSpan(0, 0, 0, 0, 200));
-
-                builder.StartSentence();
-                builder.AppendText("Every blink.");
-                builder.EndSentence();
-
-                builder.AppendBreak(new TimeSpan(0, 0, 0, 0, 500));
-
-                builder.StartSentence();
-                builder.AppendText("You can't hide from me.");
-                builder.EndSentence();
-
-                builder.AppendBreak(new TimeSpan(0, 0, 0, 0, 500));
-
-                for (int i = 0; i < 5; i++)
+                while (true)
                 {
-                    builder.AppendBreak(new TimeSpan(0, 0, 0, 0, 100));
+                    // Clear window
+                    Console.Clear();
 
-                    builder.StartStyle(new PromptStyle(PromptRate.Medium));
-                    builder.AppendText("ha ha ha ha ha");
-                    builder.EndStyle();
+                    // Get text to say
+                    Console.Write("\n Enter text to say: ");
+                    string inputText = Console.ReadLine();
+
+                    // Close app if they type exit
+                    if (inputText.ToLower() == "exit")
+                        Exit();
+                    else if (inputText.ToLower() == "open notepad")
+                    {
+                        OpenNotePad();
+                        continue;
+                    }
+
+                    Speak(inputText);
                 }
-
-                builder.AppendBreak(new TimeSpan(0, 0, 0, 0, 500));
-
-                builder.StartStyle(new PromptStyle(PromptRate.Slow));
-                builder.AppendText("I'll be back.");
-                builder.EndStyle();
-
-                SpeechSynthesizer synthesizer = new SpeechSynthesizer();
-                synthesizer.Speak(builder);
-                synthesizer.Dispose();
             }
             catch (Exception ex)
             {
 
             } 
-
-            //try
-            //{
-            //    completed = new ManualResetEvent(false);
-            //    var recognizer = new SpeechRecognitionEngine();
-
-            //    recognizer.LoadGrammar(new Grammar(new GrammarBuilder("test")) { Name = "testGrammar" }); // load a grammar
-            //    recognizer.LoadGrammar(new Grammar(new GrammarBuilder("exit")) { Name = "exitGrammar" }); // load a "exit" grammar
-            //    recognizer.SpeechRecognized += recognizer_SpeechRecognized;
-            //    recognizer.SpeechRecognitionRejected += recognizer_SpeechRecognitionRejected;
-            //    recognizer.SetInputToDefaultAudioDevice(); // set the input of the speech recognizer to the default audio device
-            //    recognizer.RecognizeAsync(RecognizeMode.Multiple); // recognize speech asynchronous
-            //    completed.WaitOne(); // wait until speech recognition is completed
-            //    recognizer.Dispose(); // dispose the speech recognition engine
-
-            //    foreach (Grammar gr in recognizer.Grammars)
-            //    {
-            //        if (gr.Name == "testGrammar")
-            //        {
-            //            recognizer.UnloadGrammar(gr);
-            //            break;
-            //        }
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-
-            //}
-
         }
 
-        static void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        private static void Exit()
         {
-            if (e.Result.Text == "test") // e.Result.Text contains the recognized text
-            {
-                Console.WriteLine("The test was successful!");
-            }
-            else if (e.Result.Text == "exit")
-            {
-                completed.Set();
-            }
+            PromptBuilder builder = new PromptBuilder();
+
+            builder.StartStyle(new PromptStyle(PromptRate.NotSet));
+            builder.AppendText("bye bye");
+            builder.EndStyle();
+
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+            synthesizer.Speak(builder);
+            synthesizer.Dispose();
+
+            Environment.Exit(0);
         }
 
-        static void recognizer_SpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        private static void Speak(string textToSay)
         {
-            if (e.Result.Alternates.Count == 0)
-            {
-                Console.WriteLine("Speech rejected. No candidate phrases found.");
-                return;
-            }
-            Console.WriteLine("Speech rejected. Did you mean:");
-            foreach (RecognizedPhrase r in e.Result.Alternates)
-            {
-                Console.WriteLine("    " + r.Text);
-            }
+            Task.Run(() => SpeakAsync(textToSay));
         }
+
+        private static async Task SpeakAsync(string textToSay)
+        {
+            // Say the text they typed
+            PromptBuilder builder = new PromptBuilder();
+
+            builder.StartSentence();
+            builder.AppendText(textToSay);
+            builder.EndSentence();
+
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+            synthesizer.Speak(builder);
+            synthesizer.Dispose();
+        }
+
+        private static void OpenNotePad()
+        {
+            Speak("opening notepad");
+
+            Process.Start("notepad.exe");
+        }
+
+        
 
     }
 }
